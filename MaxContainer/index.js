24f69652885d91070902705a05cbc63bdae37b07
Volodymyr <volodymyr.padovsky@orilinc.com>
2018-12-28 11:52:01 +0200

    implemented MaxContainer HOC

---
+++ MaxContainer/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import classNames from 'classnames';
+
+const styles = (theme) => ({
+  root: {
+    maxWidth: 1340
+  },
+  container: {}
+});
+
+const MaxContainer = (props) => {
+  const {
+    classes,
+    children,
+    spacing = 0,
+    justify = 'flex-start'
+  } = props;
+  return (
+    <div className={classNames('MaxContainer', classes.root)}>
+      <Grid
+        container
+        justify={justify}
+        spacing={spacing}
+      >
+        <Grid
+          item
+          xs={12}
+          className={classes.container}
+        >
+          {children}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+
+export default withStyles(styles)(MaxContainer);
